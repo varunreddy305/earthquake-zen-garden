f2cc545b667581f924c4b901b3e33c140855e5cc
Varun <vreddy565@gmail.com>
2021-07-21 10:17:04 -0700

    clean up

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -19,7 +18,3 @@ ReactDOM.render(
 	document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-serviceWorker.unregister();
